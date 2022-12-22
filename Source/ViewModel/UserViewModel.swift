6e0bb96b673ab73512205b27fc7932ea39b185e4
IATec - Rodrigo Leme <rodrigo.leme@iatecbr-mac034.local>
2022-12-22 15:30:59 -0300

    User view model

--- Source/ViewModel/UserViewModel.swift
+++ Source/ViewModel/UserViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+class UserViewModel {
+    private let model: UserModel
+    
+    init(model: UserModel) {
+        self.model = model
+    }
+    
+    init() {
+        self.model = UserModel()
+    }
+    
+    var email: String {
+        model.email
+    }
+    
+    func getUserFromApi(_ email: String, _ password: String, completion: @escaping(Result<UserViewModel, Error>) -> Void) {
+        let manager = UserManager(business: UserBusiness())
+        
+        manager.login(email: email, password: password) { userModel in
+            completion(.success(UserViewModel(model: userModel)))
+        } failureHandler: { error in
+            completion(.failure(error))
+        }
+    }
+    
+    func setUserFromApi(_ name: String, _ email: String, _ password: String, completion: @escaping(Result<UserViewModel, Error>) -> Void) {
+        let manager = UserManager(business: UserBusiness())
+        
+        manager.register(name: name, email: email, password: password) { userModel in
+            completion(.success(UserViewModel(model: userModel)))
+        } failureHandler: { error in
+            completion(.failure(error))
+        }
+
+    }
+}
