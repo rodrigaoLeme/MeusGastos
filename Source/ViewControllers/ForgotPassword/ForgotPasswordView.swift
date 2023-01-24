cf5b5c0225b48a67c3acd0edc63f0c8885e6b31c
IATec - Rodrigo Leme <rodrigo.leme@iatecbr-mac034.local>
2023-01-24 15:16:56 -0300

    feat: Screen of forgot password

--- Source/ViewControllers/ForgotPassword/ForgotPasswordView.swift
+++ Source/ViewControllers/ForgotPassword/ForgotPasswordView.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import TransitionButton
 
 class ForgotPasswordView: UIView {
     //MARK: Closures
+    var onSendEmailTap: ((_ email: String, _ button: TransitionButton) -> Void)?
     
     //MARK: Properts
+    lazy var imgLogin: UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage(named: "loginImage")
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.contentMode = .scaleAspectFit
+        
+        return img
+    }()
     
+    lazy var emailTextField = TextFieldDefault(placeholder: "E-mail", keyboardType: .emailAddress)
+    
+    lazy var forgotButton = ButtonPrimaryTransition(title: "Enviar E-mail")
     
     //MARK: Inits
     override init(frame: CGRect) {
@@ -21,7 +34,41 @@ class ForgotPasswordView: UIView {
     }
     
     private func setVisualElements() {
+        setImage()
+        setTextField()
+        setForgotButton()
+    }
+    
+    private func setImage() {
+        self.addSubview(imgLogin)
         
+        NSLayoutConstraint.activate([
+            imgLogin.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 10),
+            imgLogin.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            imgLogin.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0),
+        ])
+    }
+    
+    private func setTextField() {
+        self.addSubview(emailTextField)
+        
+        NSLayoutConstraint.activate([
+            emailTextField.topAnchor.constraint(equalTo: imgLogin.bottomAnchor, constant: 20),
+            emailTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24),
+            emailTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
+            emailTextField.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+    
+    private func setForgotButton() {
+        self.addSubview(forgotButton)
+        
+        NSLayoutConstraint.activate([
+            forgotButton.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            forgotButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24),
+            forgotButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
+            forgotButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
     }
     
     
